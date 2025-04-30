c41b626901dab5963a1edbfa19c2edecac468c31
Aryaman M Singha <91653939+Aryamnsls@users.noreply.github.com>
2025-04-30 22:52:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,36 +1,24 @@
-function convertToRoman(num) {
-  // Full Roman numeral map including subtractive notations
-  const romanMap = [
-    ['M', 1000],
-    ['CM', 900],
-    ['D', 500],
-    ['CD', 400],
-    ['C', 100],
-    ['XC', 90],
-    ['L', 50],
-    ['XL', 40],
-    ['X', 10],
-    ['IX', 9],
-    ['V', 5],
-    ['IV', 4],
-    ['I', 1]
-  ];
+function convertToRoman(num){
+	const romanMap = [ 
+	  ['M',1000], 
+      ['D', 500], 
+      ['C', 100], 
+      ['L', 50], 
+      ['X', 10], 
+      ['V', 5], 
+      ['I', 1]
+	];
 
-  let result = "";
+	let result = " ";
 
-  for (let i = 0; i < romanMap.length; i++) {
-    const [symbol, value] = romanMap[i];
-    while (num >= value) {
-      result += symbol;
-      num -= value;
-    }
-  }
-
-  return result;
+	for(let i = 0; i<roadMap.length; i++){
+		const [symbol, value] = romanMap[i];
+		while(num >= value){
+           result += symbol;
+			num -= value;
+		}
+	}
+	return result;
 }
 
-// Uncomment the below line to test
-// console.log(convertToRoman(36)); // Output: XXXVI
-
-// do not edit below this line
-module.exports = convertToRoman;
+module.export = convertToRoman;
