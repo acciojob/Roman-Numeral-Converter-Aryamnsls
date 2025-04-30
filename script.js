21216d554f2af3925abb369ac83d0c790130ca87
Aryaman M Singha <91653939+Aryamnsls@users.noreply.github.com>
2025-05-01 00:32:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,30 +1,27 @@
 function convertToRoman(num){
-	const romanMap = [ 
+const romanMap  = [
 	['M', 1000],
-		['CM', 900], 
-		['D', 500],
-		['CD', 400],
-		['C', 100],
-		['XC', 90],
-		['L', 50],
-		['XL', 40],
-		['X', 10],
-		['IX', 9],
-		['V', 5],
-		['IV', 4],
-		['I', 1]
-	];
-
+	['D', 500],
+	['CM', 900],
+	['CD', 400],
+	['L', 50],
+	['XC', 90],
+	['XL', 40],
+	['X', 10],
+	['IX', 9],
+	['V', 5],
+	['IV', 4],
+	['I', 1]
+];
 	let result = "";
-
-	for(let i = 0; i<romanMap.length; i++){
-		const [symbol, value] = romanMap[i];
+	for(int i = 0; i<romanMap.length(); i++){
+		const[symbol, value] = romanMap[i];
 		while(num >= value){
-           result += symbol;
+		   result += symbol;
 			num -= value;
 		}
 	}
 	return result;
-}
+	}
 
 module.exports = convertToRoman;
